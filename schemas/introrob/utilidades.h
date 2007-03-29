f80cc04d477f07b47fecd4f70d8bf1066008defc
José María Cañas <jmplaza@gsyc.es>
2007-03-29 20:31:41 +0000

    [rocapal] new schema, followball included [jmplaza] bug at introrob: introrob_mouse_x exported to let the programmers of navegacion.c know the absolute position of the point clicked with the central button of the mouse at the introrobGUI [jramos,jcaden] new release of the manual, installation section improved
    git-svn-id: http://svn.jderobot.org/jderobot/jdec/trunk@75 2fa0627c-b022-0410-acda-9783b4e515f1

--- schemas/introrob/utilidades.h
+++ schemas/introrob/utilidades.h
@@ -1,3 +1,6 @@
+extern float introrob_mouse_x, introrob_mouse_y;
+/* absolute position of the point clicked with the central mouse button */
+
 int absolutas2relativas(Tvoxel in, Tvoxel *out);
 int relativas2absolutas(Tvoxel in, Tvoxel *out);
 int pintaSegmento(Tvoxel a, Tvoxel b, int color);
