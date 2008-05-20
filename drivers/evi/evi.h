9d7ab4078f8ed1e71b87394df88a9183e6ac403f
Roberto Calvo <rocapal@gsyc.urjc.es>
2008-05-20 21:47:52 +0000

    eliminamos trunk
    git-svn-id: http://svn.jderobot.org/jderobot/jdec/trunk@178 2fa0627c-b022-0410-acda-9783b4e515f1

--- drivers/evi/evi.h
+++
@@ -1,5 +0,0 @@
-#ifndef EVI_H
-#define EVI_H
-extern "C" void evi_startup(char *configfile);
-extern "C" void evi_close();
-#endif
--- schemas/introrob/variables.h
+++
@@ -1,4 +0,0 @@
-float v;
-float w;
-float jde_robot[5];
-float jde_laser[NUM_LASER];
