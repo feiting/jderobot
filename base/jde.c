313bd80ac9124d93a252d800ad4f73d6a7ee9504
Roberto Calvo <rocapal@gsyc.urjc.es>
2008-11-16 13:08:41 +0000

    Add new license header with GPL v3.0 (base and libx directories)
    git-svn-id: http://svn.jderobot.org/jderobot/jdec/trunk@250 2fa0627c-b022-0410-acda-9783b4e515f1

--- base/jde.c
+++ base/jde.c
@@ -1,21 +1,22 @@
 /*
- *  Copyright (C) 2006 José María Cañas Plaza
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  Copyright (C) 1997-2008 JDE Developers Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  along with this program.  If not, see http://www.gnu.org/licenses/. 
  *
  *  Authors : José María Cañas Plaza <jmplaza@gsyc.escet.urjc.es>
+ *
  */
 
 #define thisrelease "jdec 4.3-svn"
--- libs/progeo/progeo.c
+++ libs/progeo/progeo.c
@@ -1,8 +1,24 @@
-/* progeo 0.2 modified by Antonio Pineda
-   
-PROJECT and BACKPROJECT functions not implemented yet for stereocameras
-	
-*/
+/*
+ *
+ *  Copyright (C) 1997-2008 JDE Developers Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/. 
+ *
+ *  Authors : Jose María Cañas Plaza <jmplaza@gsyc.es>
+ *            Antonio Pineda
+ */
+
 #include "progeo.h"
 #include <math.h>
 #include <stdio.h>
