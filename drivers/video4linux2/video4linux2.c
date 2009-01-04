3c0e2bbfc8ffc8c351a5a59f72f17fd907a3832d
Roberto Calvo <rocapal@gsyc.urjc.es>
2009-01-04 14:47:49 +0000

    change license header to GPL v3.0
    git-svn-id: http://svn.jderobot.org/jderobot/jdec/trunk@270 2fa0627c-b022-0410-acda-9783b4e515f1

--- drivers/video4linux2/video4linux2.c
+++ drivers/video4linux2/video4linux2.c
@@ -1,28 +1,22 @@
 /*
- *  Copyright (C) 2007 Jose Maria Cañas Plaza
  *
- *  This program is free software; you can redistribute it and/or modify
+ *  Copyright (C) 1997-2008 JDE Developers Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- */
-
-/**
- *  jdec video4linux2 driver provides video images to color variables from video4linux2 devices, such as USB or BT878 cameras.
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
  *
- *  @file video4linux2.c
- *  @author Antonio Pineda Cabello <apineda@gsyc.escet.urjc.es> and Jose Maria Ca�as Plaza <jmplaza@gsyc.escet.urjc.es>
- *  @version 4.1
- *  @date 30-05-2007
+ *  Authors : Jose Maria Cañas <jmplaza@gsyc.escet.urjc.es>
+ *
  */
 
 #include <stdio.h>
--- services/networkserver/jdemessages.h
+++ services/networkserver/jdemessages.h
@@ -1,3 +1,28 @@
+/*
+ *
+ *  Copyright (C) 1997-2008 JDE Developers Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ *  Authors : Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
+ *            Javier Martin Ramos <xaverbrennt@yahoo.es>
+ *            Jose Maria Cañas <jmplaza@gsyc.escet.urjc.es>
+ *
+ *
+ */
+
+
 /* 
  Communications always starts with the client sending its own name, a string of MAX_CLIENT_NAME chars, to the server.
 
