abe921d2d8d04ea7f1a9124d127b5dccd9f49512
José María Cañas <jmplaza@gsyc.es>
2009-03-21 10:02:23 +0000

    [eperdes] ticket#235 fixed. Opencvdemo schema now includes Hough transform to detect lines in images.
    git-svn-id: http://svn.jderobot.org/jderobot/jdec/trunk@316 2fa0627c-b022-0410-acda-9783b4e515f1

--- schemas/opencvdemo/opencvdemo.c
+++ schemas/opencvdemo/opencvdemo.c
@@ -54,6 +54,8 @@ int radio_sobel=0;
 int radio_opflow=0;
 int radio_pyramid=0;
 int radio_convol=0;
+int radio_houghs=0;
+int radio_houghp=0;
 
 int opflow_first=1;
 IplImage *previous;
@@ -71,7 +73,10 @@ int modulo = 1;
 int offset = 0;
 float * kernel = NULL;
 
-float threshold = 1;
+float threshold = 50; //Canny
+float threshold2 = 80; //Hough
+float length = 50;
+float maxgap = 10;
 
 /*Global variables*/
 char *image;
@@ -177,6 +182,30 @@ void on_active_convol_toggled (GtkCheckMenuItem *menu_item, gpointer user_data){
 		radio_convol=0;
 	else {
 		radio_convol=1;
+	}
+}
+
+void on_active_houghs_toggled (GtkCheckMenuItem *menu_item, gpointer user_data){
+	if(radio_houghs) {
+		radio_houghs=0;
+		gtk_widget_hide(glade_xml_get_widget(xml, "tableCanny"));
+		gtk_widget_hide(glade_xml_get_widget(xml, "tableHough"));
+	} else { 
+		radio_houghs=1;
+		gtk_widget_show(glade_xml_get_widget(xml, "tableCanny"));
+		gtk_widget_show(glade_xml_get_widget(xml, "tableHough"));
+	}
+}
+
+void on_active_houghp_toggled (GtkCheckMenuItem *menu_item, gpointer user_data){
+	if(radio_houghp) {
+		radio_houghp=0;
+		gtk_widget_hide(glade_xml_get_widget(xml, "tableCanny"));
+		gtk_widget_hide(glade_xml_get_widget(xml, "tableHough"));
+	} else { 
+		radio_houghp=1;
+		gtk_widget_show(glade_xml_get_widget(xml, "tableCanny"));
+		gtk_widget_show(glade_xml_get_widget(xml, "tableHough"));
 	}
 }
 
@@ -534,6 +563,101 @@ void convolution() {
 	cvReleaseImage(&dst);
 }
 
+void hough_standard() {
+
+	IplImage* src;
+	IplImage *gray; 
+	IplImage* dst;
+	IplImage* color_dst;
+	CvMemStorage* storage;
+	CvSeq* lines;
+	int i;
+	
+	src = cvCreateImage(cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 3);
+	gray = cvCreateImage(cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 1);
+ 	dst= cvCreateImage( cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 1 );
+	color_dst = cvCreateImage( cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 3);
+	storage = cvCreateMemStorage(0);
+	lines = 0;
+
+	memcpy(src->imageData, image, src->width*src->height*src->nChannels);
+
+	/*Canny filter*/
+	cvCvtColor(src, gray, CV_RGB2GRAY);	
+	cvCanny( gray, dst, threshold, threshold*3, 3 );
+
+	cvCvtColor( dst, color_dst, CV_GRAY2RGB );
+
+	/*Hough probabilistic*/										/*Distance, angle, threshold, not used, not used*/
+	lines = cvHoughLines2( dst, storage, CV_HOUGH_STANDARD, 1, CV_PI/180, threshold2, 0, 0);
+
+	/*Draw lines*/
+	for( i = 0; i < MIN(lines->total,100); i++ ) {
+		float* line = (float*)cvGetSeqElem(lines,i);
+		float rho = line[0];
+		float theta = line[1];
+		CvPoint pt1, pt2;
+		double a = cos(theta), b = sin(theta);
+		double x0 = a*rho, y0 = b*rho;
+		pt1.x = cvRound(x0 + 1000*(-b));
+		pt1.y = cvRound(y0 + 1000*(a));
+		pt2.x = cvRound(x0 - 1000*(-b));
+		pt2.y = cvRound(y0 - 1000*(a));
+		cvLine( color_dst, pt1, pt2, CV_RGB(255,0,0), 3, 8, 0 );
+	}
+
+	memcpy(image_aux, color_dst->imageData, src->width*src->height*src->nChannels); 	
+
+	cvReleaseImage(&src);
+	cvReleaseImage(&gray);
+	cvReleaseImage(&dst);
+	cvReleaseImage(&color_dst);
+
+
+}
+
+void hough_probabilistic() {
+
+	IplImage* src;
+	IplImage *gray; 
+	IplImage* dst;
+	IplImage* color_dst;
+	CvMemStorage* storage;
+	CvSeq* lines;
+	int i;
+	
+	src = cvCreateImage(cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 3);
+	gray = cvCreateImage(cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 1);
+ 	dst= cvCreateImage( cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 1 );
+	color_dst = cvCreateImage( cvSize((*myAA).width,(*myAA).height), IPL_DEPTH_8U, 3);
+	storage = cvCreateMemStorage(0);
+	lines = 0;
+
+	memcpy(src->imageData, image, src->width*src->height*src->nChannels);
+
+	/*Canny filter*/
+	cvCvtColor(src, gray, CV_RGB2GRAY);	
+	cvCanny( gray, dst, threshold, threshold*3, 3 );
+
+	cvCvtColor( dst, color_dst, CV_GRAY2RGB );
+
+	/*Hough probabilistic*/										/*Distance, angle, threshold, min length, max gap*/
+	lines = cvHoughLines2( dst, storage, CV_HOUGH_PROBABILISTIC, 1, CV_PI/180, threshold2, length, maxgap);
+
+	/*Draw lines*/
+	for( i = 0; i < lines->total; i++ ) {
+		CvPoint* line = (CvPoint*)cvGetSeqElem(lines,i);
+		cvLine(color_dst, line[0], line[1], CV_RGB(255,0,0), 3, 8, 0);
+	}
+
+	memcpy(image_aux, color_dst->imageData, src->width*src->height*src->nChannels); 	
+
+	cvReleaseImage(&src);
+	cvReleaseImage(&gray);
+	cvReleaseImage(&dst);
+	cvReleaseImage(&color_dst);
+}
+
 void opencvdemo_iteration(){
 	int i;
 
@@ -566,6 +690,10 @@ void opencvdemo_iteration(){
 			pyramid();
 		else if(radio_convol)
 			convolution();
+		else if(radio_houghs) 
+			hough_standard();
+		else if(radio_houghp) 
+			hough_probabilistic();
 	}
 	pthread_mutex_unlock(&main_mutex);
 }
@@ -971,6 +1099,12 @@ void opencvdemo_guidisplay(){
 			threshold = gtk_range_get_value(glade_xml_get_widget(xml, "threshold"));
 		}
 
+		if(radio_houghs || radio_houghp){
+			threshold2 = gtk_range_get_value(glade_xml_get_widget(xml, "threshold2"));
+			length = gtk_range_get_value(glade_xml_get_widget(xml, "length"));
+			maxgap = gtk_range_get_value(glade_xml_get_widget(xml, "maxgap"));
+		}
+
 		gdk_threads_leave();
    }
    pthread_mutex_unlock(&main_mutex);
@@ -1031,6 +1165,10 @@ void opencvdemo_show(void){
                           "toggled", G_CALLBACK(on_active_pyramid_toggled), NULL);
          g_signal_connect(GTK_WIDGET(glade_xml_get_widget(xml, "radio_convol")),
                           "toggled", G_CALLBACK(on_active_convol_toggled), NULL);
+         g_signal_connect(GTK_WIDGET(glade_xml_get_widget(xml, "radio_houghs")),
+                          "toggled", G_CALLBACK(on_active_houghs_toggled), NULL);
+         g_signal_connect(GTK_WIDGET(glade_xml_get_widget(xml, "radio_houghp")),
+                          "toggled", G_CALLBACK(on_active_houghp_toggled), NULL);
       }
 
 		/*Set values to sliders*/
@@ -1041,10 +1179,14 @@ void opencvdemo_show(void){
 		gtk_range_set_value(glade_xml_get_widget(xml, "vmax"),vmax);
 		gtk_range_set_value(glade_xml_get_widget(xml, "vmin"),vmin);
 		gtk_range_set_value(glade_xml_get_widget(xml, "threshold"),threshold);
+		gtk_range_set_value(glade_xml_get_widget(xml, "threshold2"),threshold2);
+		gtk_range_set_value(glade_xml_get_widget(xml, "length"),length);
+		gtk_range_set_value(glade_xml_get_widget(xml, "maxgap"),maxgap);
 
 		/*Hide frames until they are displayed*/
 		gtk_widget_hide(glade_xml_get_widget(xml, "tableColor"));
 		gtk_widget_hide(glade_xml_get_widget(xml, "tableCanny"));
+		gtk_widget_hide(glade_xml_get_widget(xml, "tableHough"));
 
       if (win==NULL){
          fprintf(stderr, "Error loading graphical interface\n");
